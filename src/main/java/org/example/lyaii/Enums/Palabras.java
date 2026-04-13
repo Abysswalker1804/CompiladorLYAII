ef9aa5f91f2892e828e5531125f10fc4dc15d05d
Tadeo Palma <tadeo.palavi@gmail.com>
2026-04-12 21:03:31 -0600

    Enums 12/04/2026

---
+++ src/main/java/org/example/lyaii/Enums/Palabras.java
@@ -0,0 +1,20 @@
+package org.example.lyaii.Enums;
+
+public enum Palabras {
+    NOTAWORD,
+    PR01,
+    PR02,
+    PR03,
+    PR04,
+    PR05,
+    PR06,
+    PR07,
+    PR08,
+    PR09,
+    PR10,
+    PR11,
+    PR12,
+    PR13,
+    PR14,
+    PR15
+}
